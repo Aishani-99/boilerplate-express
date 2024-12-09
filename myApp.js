73371cdcd798655dc0083215e94d5ca26168a06d
Aishani-99 <aishani9199@gmail.com>
2024-12-10 02:06:35 +0530

    edited again

--- myApp.js
+++ myApp.js
@@ -1,13 +1,3 @@
 let express = require('express');
 let app = express();
-
-app.get('/', () => {
-    console.log("Hello World"); 
-    // res.send("");
-});
-
-
-let port = 3000; 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+console.log("Hello World"); 
